81dbbd6cabc9de2f53e8145e713fd1a9f3b80b19
lenysi4ka25 <lenysi4ka@mail.ru>
2022-08-24 17:29:16 +0300

    Невозможное - возможно?!

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        var Boxer1 = 78.2;
+        var Boxer2 = 82.7;
+        var DifferentiaInterPonderaPugiles = Boxer2 - Boxer1;
+        System.out.println("Разница между весами бойцов: " + DifferentiaInterPonderaPugiles + " кг.");
+        var ReliquumDivisio = Boxer2 % Boxer1;
+        System.out.println("Остаток от деления " + ReliquumDivisio);
+    }
+}
